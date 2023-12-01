e7276cc0e0c81d9378b799324e17fcb69ff3f437
dcoder15 <david.tomic11@gmail.com>
2023-12-01 21:20:06 +0100

    fix: navbar

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -2,52 +2,59 @@ import React from "react";
 import { Link, makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        margin: "0 10px"
-    }
+	root: {
+		margin: "0 10px",
+	},
 }));
 
 /**
  * Props passed to the @see NavLink.
  */
 interface INavLinkProps {
-    /**
-     * Class name.
-     */
-    className?: string;
+	/**
+	 * Class name.
+	 */
+	className?: string;
 
-    /**
-     * Href.
-     */
-    href: string;
+	/**
+	 * Href.
+	 */
+	href: string;
 
-    /**
-     * Link text.
-     */
-    text: string;
+	/**
+	 * Link text.
+	 */
+	text: string;
 
-    /**
-     * Executed when clicked.
-     */
-    onClick?: () => void;
+	/**
+	 * Executed when clicked.
+	 */
+	onClick?: () => void;
 }
 
 /**
  * Navigation link.
  */
-const NavLink: React.FunctionComponent<INavLinkProps> = (props: INavLinkProps) => {
-    const classes = useStyles();
+const NavLink: React.FunctionComponent<INavLinkProps> = (
+	props: INavLinkProps
+) => {
+	const classes = useStyles();
 
-    const { href, text, className, onClick } = props;
+	const { href, text, className, onClick } = props;
 
-    return <Link
-        className={className ? `${classes.root} ${className}` : classes.root}
-        variant={"button"}
-        color={"inherit"}
-        href={href}
-        onClick={() => onClick && onClick()}>
-        {text}
-    </Link>;
-}
+	return (
+		<Link
+			className={
+				className ? `${classes.root} ${className}` : classes.root
+			}
+			variant={"button"}
+			color={"inherit"}
+			href={href}
+			onClick={() => onClick && onClick()}
+		>
+			{text}
+		</Link>
+	);
+};
 
 export default NavLink;
--- src/elements/Education.tsx
+++ src/elements/Education.tsx
@@ -12,76 +12,77 @@ import InfoIcon from "@material-ui/icons/Info";
 import { MilestoneDetails } from "../static/text_content";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-    },
-    divider: {
-        margin: "20px 0",
-        width: "100%"
-    }
+	root: {},
+	divider: {
+		margin: "20px 0",
+		width: "100%",
+	},
 }));
 
 /**
  * Props passed to the @see Education.
  */
-interface IEducationProps {
-}
+interface IEducationProps {}
 
 /**
  * Education.
  */
-const Education: React.FunctionComponent<IEducationProps> = (props: IEducationProps) => {
-    const classes = useStyles();
+const Education: React.FunctionComponent<IEducationProps> = (
+	props: IEducationProps
+) => {
+	const classes = useStyles();
 
-    const appStore = ContainerFactory.getContainer().resolve<AppStore>("AppStore");
-    const storeState = useStoreState(appStore);
+	const appStore =
+		ContainerFactory.getContainer().resolve<AppStore>("AppStore");
+	const storeState = useStoreState(appStore);
 
-    if (!storeState) {
-        return null;
-    }
+	if (!storeState) {
+		return null;
+	}
 
-    const { localized_text_content } = storeState;
+	const { localized_text_content } = storeState;
 
-    return <React.Fragment>
-        <Box
-            className={classes.root}
-            id={"education"}>
-            <Grid
-                container
-                direction={"column"}>
-                <Grid item>
-                    <Typography
-                        variant={"h6"}
-                        gutterBottom>
-                        {localized_text_content.educationHeading}
-                    </Typography>
-                </Grid>
-                <Grid item>
-                    <MilestonesContainer>
-                        {(localized_text_content.educations as MilestoneDetails[]).map(education => {
-                            return <Milestone
-                                key={education.id}
-                                heading={education.duration}>
-                                <IconAndText
-                                    icon={<PlaceIcon />}
-                                    text={education.details.name}
-                                />
-                                <IconAndText
-                                    icon={<InfoIcon />}
-                                    text={education.details.program}
-                                />
-                                <IconAndText
-                                    icon={<SchoolIcon />}
-                                    text={education.details.degree}
-                                />
-                            </Milestone>
-                        })
-                        }
-                    </MilestonesContainer>
-                </Grid>
-            </Grid>
-        </Box>
-        <Divider className={classes.divider} />
-    </React.Fragment>;
-}
+	return (
+		<React.Fragment>
+			<Box className={classes.root} id={"education"}>
+				<Grid container direction={"column"}>
+					<Grid item>
+						<Typography variant={"h6"} gutterBottom>
+							{localized_text_content.educationHeading}
+						</Typography>
+					</Grid>
+					<Grid item>
+						<MilestonesContainer>
+							{(
+								localized_text_content.educations as MilestoneDetails[]
+							).map((education) => {
+								return (
+									<Milestone
+										key={education.id}
+										heading={education.duration}
+									>
+										<IconAndText
+											icon={<PlaceIcon />}
+											text={education.details.name}
+										/>
+										<IconAndText
+											icon={<InfoIcon />}
+											text={education.details.program}
+										/>
+										<IconAndText
+											icon={<SchoolIcon />}
+											text={education.details.degree}
+										/>
+									</Milestone>
+								);
+							})}
+						</MilestonesContainer>
+					</Grid>
+				</Grid>
+			</Box>
+			<Divider className={classes.divider} />
+		</React.Fragment>
+	);
+};
 
 export default Education;
